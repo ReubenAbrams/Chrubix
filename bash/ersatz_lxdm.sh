4147a3dcee845cb1be71f76c6ea0756abcb00350
Reuben Abrams <reuben_abrams@yahoo.com>
2014-06-16 21:59:07 -0700

    Added ersatz_lxdm.sh

---
+++ bash/ersatz_lxdm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# ersatz_lxdm.sh
+# - calls chrubix.sh w/ ersatz_lxdm as param #1
+#
+#################################################################################
+
+
+chrubix.sh ersatz_lxdm $@
+exit $?
