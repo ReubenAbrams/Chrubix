743f07a0cc8aa02e0b8959f82df09ff40f88e66c
Reuben Abrams <reuben_abrams@yahoo.com>
2014-06-16 21:52:00 -0700

    Added ersatz_lxdm.sh (again)

--- bash/chrubix_stage1.sh
+++ bash/chrubix_stage1.sh
@@ -216,7 +216,7 @@ install_chrubix() {
 	wget $CHRUBIX_URL -O - | tar -zx -C $root/usr/local/bin
 	mv $root/usr/local/bin/Chrubix* $root/usr/local/bin/Chrubix	# rename Chrubix-master (or whatever) to Chrubix
 	wget $OVERLAY_URL -O - | tar -Jx -C $root/usr/local/bin/Chrubix || failed "Sorry. Dropbox is down. We'll have to rely on GitHub..."
-	for f in chrubix.sh greeter.sh CHRUBIX redo_mbr.sh modify_sources.sh ; do
+	for f in chrubix.sh greeter.sh ersatz_lxdm.sh CHRUBIX redo_mbr.sh modify_sources.sh ; do
 		ln -sf Chrubix/bash/$f $root/usr/local/bin/$f
 	done
 
