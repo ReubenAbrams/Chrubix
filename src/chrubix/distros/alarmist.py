1885c1efe2ebf54bcab19173f02cbf444b95a978
Reuben Abrams <reuben_abrams@yahoo.com>
2015-04-23 11:41:17 -0700

    Added alarmist (no longer used!) to pkg

--- src/chrubix/distros/alarmist.py
+++ src/chrubix/distros/alarmist.py
@@ -5,7 +5,7 @@
 
 
 from chrubix.distros.debian import WheezyDebianDistro
-from chrubix.utils import disable_root_password
+from chrubix.utils import disable_root_password, failed
 
 
 class AlarmistDistro( WheezyDebianDistro ):
@@ -14,6 +14,7 @@ class AlarmistDistro( WheezyDebianDistro ):
     def __init__( self , *args, **kwargs ):
         super( AlarmistDistro, self ).__init__( *args, **kwargs )
         self.name = 'alarmist'
+        failed( 'WE NO LONGER USE ALARMIST DISTRO' )
         # self.name remains 'wheezy' because... well, it's all so much simpler that way
 
     def migrate_or_squash_OS( self ):
