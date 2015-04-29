bfa776e4c4ba66adcdeb931f7db525edef0dbec9
Reuben Abrams <reuben_abrams@yahoo.com>
2015-04-29 10:17:02 -0700

    Use vivid instead of pangolin Use init() [not initial declaration] to declare/modify class instance vars

--- src/chrubix/distros/ubuntu.py
+++ src/chrubix/distros/ubuntu.py
@@ -2,37 +2,31 @@
 #
 # ubuntu.py
 #
-
 # FIXME - see http://marcin.juszkiewicz.com.pl/2013/02/14/how-to-install-ubuntu-13-04-on-chromebook/
 
-from chrubix.utils import wget, system_or_die, unmount_sys_tmp_proc_n_dev, mount_sys_tmp_proc_n_dev, logme, chroot_this, g_proxy
+
+from chrubix.utils import wget, system_or_die, unmount_sys_tmp_proc_n_dev, \
+                          mount_sys_tmp_proc_n_dev, logme, chroot_this, g_proxy, failed, \
+                          write_oneliner_file, do_a_sed
 from chrubix.distros.debian import JessieDebianDistro
 
 
-class UbuntuDistro( JessieDebianDistro ):
-    important_packages = JessieDebianDistro.important_packages + ' linux-chromebook'
-    final_push_packages = JessieDebianDistro.final_push_packages + ' lxdm wmsystemtray'
+class UbuntuDistro( JessieDebianDistro ):  # FIXME try DebianDistro instead...?
     def __init__( self , *args, **kwargs ):
         super( UbuntuDistro, self ).__init__( *args, **kwargs )
         self.name = 'ubuntu'
+        self.branch = 'UNKNOWN_BRANCH'
+        self.packages_folder_url = 'http://ports.ubuntu.com/ubuntu-ports/'
+        assert( self.important_packages not in ( '', None ) )
+        self.important_packages += ' linux-chromebook'
+        self.final_push_packages += ' lxdm wmsystemtray'
 
     def install_aircrack_ng( self ):
-        self.build_and_install_package_from_ubuntu_source( 'aircrack-ng' )
+        logme( 'FYI, I am not installing aircrack' )
+#        self.build_and_install_package_from_ubuntu_source( 'aircrack-ng' )
 #        for stub in ('debian.tar', '.dsc', '.orig.tar'):
 #            relevant_line = call_binary( ['curl','http://ubuntu2.cica.es/ubuntu/ubuntu/pool/universe/a/aircrack-ng/'] )[1].strip()
 #            print('%s ==> %s' % (stub, relevant_line))
-
-    def install_barebones_root_filesystem( self ):
-        logme( 'Ubuntu - install_barebones_root_filesystem() - starting' )
-        unmount_sys_tmp_proc_n_dev( self.mountpoint )
-        wget( url = 'https://dl.dropboxusercontent.com/u/59916027/chrubix/skeletons/ubuntu.tar.xz', extract_to_path = self.mountpoint, decompression_flag = 'J', title_str = self.title_str, status_lst = self.status_lst )
-        mount_sys_tmp_proc_n_dev( self.mountpoint )
-        return 0
-
-    def install_locale( self ):
-        logme( 'UbuntuDistro - install_locale() - starting' )
-        self.do_generic_locale_configuring()
-        logme( 'UbuntuDistro - install_locale() - leaving' )
 
     def install_debianspecific_package_manager_tweaks( self, yes_add_ffmpeg_repo = False ):
         logme( 'UbuntuDistro - install_package_manager_tweaks() - starting' )
@@ -50,15 +44,51 @@ Acquire::https::Proxy "https://%s/";
             f.close()
         logme( 'UbuntuDistro - install_package_manager_tweaks() - leaving' )
 
-class PangolinUbuntuDistro( UbuntuDistro ):
+class VividUbuntuDistro( UbuntuDistro ):
     def __init__( self , *args, **kwargs ):
-        super( PangolinUbuntuDistro, self ).__init__( *args, **kwargs )
-        self.branch = 'pangolin liblzo2-2 liblzo2-dev dillo'
-        for r in 'aircrack-ng gobby pv bc busybox rng-tools python-pip mpg123 pavucontrol mplayer claws-mail bluez-utils keepassx exo-utils'.split( ' ' ):
+        super( VividUbuntuDistro, self ).__init__( *args, **kwargs )
+        self.branch = 'vivid'  # '15.04'  # vivid, a.k.a. latest
+        self.important_packages = 'x11-server-utils x11-kb-utils x11-apps ' + self.important_packages + ' liblzo2-2 liblzo2-dev dillo'
+        for r in 'libreoffice openjdk-8-jre ttf-dejavu ttf-liberation firmware-libertas liferea aircrack-ng bc busybox rng-tools python-pip mpg123 python-software-properties openjdk-8-jre pavucontrol mplayer claws-mail bluez-utils keepassx exo-utils'.split( ' ' ):
             self.important_packages = self.important_packages.replace( r + ' ', '' )
 
 
     def install_important_packages( self ):
-        self.install_all_important_packages_other_than_systemd_sysv()
+        # https://wiki.ubuntu.com/DebootstrapChroot
+        for cmd in ( 
+                    'apt-get update',
+                    'apt-get --no-install-recommends install wget debconf devscripts gnupg nano',
+                    'apt-get update',
+                    'apt-get install locales dialog',
+                    'locale-gen en_US.UTF-8',
+                    'add-apt-repository ppa:ubuntuhandbook1/apps',
+                    'apt-get update',
+                    'apt-get install liferea' ):
+            chroot_this( self.mountpoint, 'yes "" | ' + cmd, title_str = self.title_str, status_lst = self.status_lst )
+#        failed( 'Aborting for test porpoises' )
+#        'tzselect; TZ='Continent/Country'; export TZ
+        super( VividUbuntuDistro, self ).install_important_packages()  # FIXME yes_add_ffmpeg_repo = True )
         self.install_aircrack_ng()
+
+    def install_i2p( self ):
+        failed( 'https://launchpad.net/~i2p-maintainers/+archive/ubuntu/i2p' )
+#        chroot_this( self.mountpoint, 'wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2014.2_all.deb -O - > /tmp/debmult.deb', attempts = 1, title_str = self.title_str, status_lst = self.status_lst )
+#        chroot_this( self.mountpoint, 'dpkg -i /tmp/debmult.deb', attempts = 1, title_str = self.title_str, status_lst = self.status_lst )
+        assert( self.branch in ( 'wheezy', 'jessie' ) )
+        write_oneliner_file( '%s/etc/apt/sources.list.d/i2p.list' % ( self.mountpoint ), '''
+deb http://deb.i2p2.no/ %s main
+deb-src http://deb.i2p2.no/ %s main
+''' % ( self.branch, self.branch ) )
+        for cmd in ( 
+                        'yes 2>/dev/null | add-apt-repository "deb http://deb.i2p2.no/ %s main"' % ( self.branch ),
+                        'yes "" 2>/dev/null | curl https://geti2p.net/_static/i2p-debian-repo.key.asc | apt-key add -',
+                        'yes 2>/dev/null | apt-get update'
+                   ):
+            chroot_this( self.mountpoint, cmd, title_str = self.title_str, status_lst = self.status_lst,
+                             on_fail = "Failed to run %s successfully" % ( cmd ) )
+        chroot_this( self.mountpoint, 'yes | apt-get install i2p i2p-keyring', on_fail = 'Failed to install i2p' )
+        logme( 'tweaking i2p ID...' )
+        do_a_sed( '%s/etc/passwd' % ( self.mountpoint ), 'i2p:/bin/false', 'i2p:/bin/bash' )
+
+
 
